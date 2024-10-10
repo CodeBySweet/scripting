e7a68b4ea8f59d1cce680257b2d2789d3889bf62
Shirin Z. <testdevops77@gmail.com>
2024-10-10 01:46:22 +0000

    Python session 1

---
+++ python/function.py
@@ -0,0 +1,8 @@
+
+def sum(a, b):
+    result = a + b
+    return result
+arg1 = int(input("PLease enter first number: "))
+arg2 = int(input("PLease enter second number: "))
+result = sum(arg1, arg2)
+print(result)
---
+++ python/helloworld.py
@@ -0,0 +1,26 @@
+## function(argument)
+
+message = "Hello Wolrd"
+print(message)
+# print("Hello Wolrd")
+
+if 5 > 2:
+    print("5 is greater than 2")
+elif 10 > 2:
+    print("10 is greater than 5")
+else:
+    print("Something is wrong")
+
+for item in ["Shirin", "Maryam", "Ilhom"]:
+    if item == "Shirin":
+        name = "Shirin"
+
+x = 0
+while x < 5:
+    print("Welcome", name, x, "times")
+    x = x + 1
+
+
+
+    
+
